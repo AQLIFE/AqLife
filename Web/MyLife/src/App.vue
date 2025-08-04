21744248c7daae8a6f1f550cc82fa5f34e7d6caf
Yuto <aqlife@163.com>
2025-08-04 22:30:08 +0800

    重写AuthorCard组件,由ElCard组件转写成更贴近原生的写法,保证一定的兼容性

--- Web/MyLife/src/App.vue
+++ Web/MyLife/src/App.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
 import {  ElCol, ElContainer, ElHeader, ElRow } from 'element-plus';
 import { RouterView } from 'vue-router';
+import AuthorCard from '@/components/AuthorCard.vue'
 </script>
 
 <template>
 	<ElContainer id="Mylife">
-		<ElCol id="left" :span="6"></ElCol>
+		<ElCol id="left" :span="6">
+			<ElRow>
+				<ElCol class="lcard"><AuthorCard/></ElCol>
+			</ElRow>
+
+		</ElCol>
 		<ElCol :span="18" id="right">
 			<ElHeader height="120">Header</ElHeader>
 			<ElCol>
@@ -19,13 +25,13 @@ import { RouterView } from 'vue-router';
 #Mylife {
 	width: 100vw;
 	height: 100vh;
-	;
 }
 
 #left {
 	background-color: var(--back_color_lv1);
 }
 
+.lcard{padding:4vw;}
 #right{
 	background-color: var(--back_color_lv2);
 }
--- Web/MyLife/src/components/AuthorCard.vue
+++ Web/MyLife/src/components/AuthorCard.vue
@@ -1,13 +1,47 @@
 <template>
-<ElCard>
-    <template #header>
-        <ElAvatar src="https://example.com/avatar.jpg" />
-        <span>Author Name</span>
-    </template>
-    
-</ElCard>
+    <ElCol id="author">
+        <ElRow flex="row" wrap="nowrap" justify="start" align="middle">
+            <ElCol id="avatar" :span="8">
+                <ElImage src="/src/assets/b_6584376235e266a00019954e8c2e7d26.jpg" />
+            </ElCol>
+            <ElCol id="info" :span="16">
+                <ElRow flex="column" wrap="nowrap">
+                    <ElCol id="name">AQ</ElCol>
+                    <ElDivider title="个人概述" />
+                    <ElCol id="desc">喜欢Web,虽然只是略懂</ElCol>
+                </ElRow>
+            </ElCol>
+        </ElRow>
+    </ElCol>
 </template>
 
 <script lang="ts" setup>
-import { ElCard,ElAvatar } from 'element-plus';
+import { ElCard, ElImage, ElRow, ElCol, ElDivider } from 'element-plus';
 </script>
+
+<style scoped>
+#author {
+    background-color: white;
+    overflow: hidden;
+}
+
+#author .el-row{
+    height: initial;
+}
+
+#avatar{font-size: 0;}
+
+
+
+#info{display: flex;flex-direction:column;min-height: 2vw;padding: 0;margin: 0;}
+#info .el-row{padding: 0;margin: 0;height: auto;display: flex;flex-direction: column;gap:0;}
+#name {
+    padding-left: 20px;
+    text-align: left;
+    font-size: 1rem;
+    padding-bottom: 20px;
+}
+
+#desc{font-size: 0.7rem; padding: 5px;}
+.el-divider{padding: 0;margin: 0;}
+</style>
