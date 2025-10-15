f0742ffa261e04f43a461fdfc44176260a7d6df3
Yuto <aqlife@163.com>
2025-10-15 23:38:11 +0800

    test

--- Web/MyLife/src/main.ts
+++ Web/MyLife/src/main.ts
@@ -1,13 +1,13 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import {  createSSRApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import { router } from '@/services/router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
-const app = createApp(App)
+const app = createSSRApp(App)
 
 app.use(createPinia()).use(ElementPlus).use(router)
 
