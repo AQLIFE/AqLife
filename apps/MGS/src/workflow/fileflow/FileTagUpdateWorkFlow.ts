6e3ab44f42ebca2e5a3cac499c820809795f4ec0
AQ <93851495+AQLIFE@users.noreply.github.com>
2026-09-24 21:37:22 +0800

    fix(MGS): 修复 File 分页结果读取

--- apps/MGS/src/workflow/fileflow/FileTagUpdateWorkFlow.ts
+++ apps/MGS/src/workflow/fileflow/FileTagUpdateWorkFlow.ts
@@ -1,5 +1,6 @@
+
 import type { FileApi, FileDto, TagDto, UpdateFileTagCommand } from "@/api"
-import type { FileStore } from '@/stores/useFileStore.ts'
+import type { FileStore } from '@/stores/useFileStore'
 
 export class FileTagUpdateWorkflow {
   constructor(
@@ -7,46 +8,45 @@ export class FileTagUpdateWorkflow {
     private readonly value: TagDto[],
     private readonly api: FileApi,
     private readonly store: FileStore
-  ) { }
-  async run():Promise<FileDto> {
+  ) {}
 
+  async run(): Promise<FileDto> {
     const tagIds = this.extractTags(this.value)
-
-    const command = this.buildCommand(tagIds)
-
-    const result = await this.execute(command)
-
+    const result = await this.execute(this.buildCommand(tagIds))
     const validateDto = await this.validate(result)
-
     this.commit(validateDto)
-    return validateDto;
+    return validateDto
   }
 
   private extractTags(tags: TagDto[]): string[] {
     return tags.map(x => x.uid).filter((uid): uid is string => !!uid)
   }
 
-  private buildCommand(tagIds: string[]) {
+  private buildCommand(tagIds: string[]): UpdateFileTagCommand {
     return {
       uid: this.file.uid,
-      tags: tagIds
+      tags: tagIds,
     }
   }
 
   private async execute(command: UpdateFileTagCommand) {
-    return await this.api.apiFileTagPatch({ updateFileTagCommand: command })
+    return this.api.apiFileTagPatch({ updateFileTagCommand: command })
   }
 
   private async validate(result: string) {
-    const latestDtos = await this.api.apiFileGet({ uID: result })
-    if (!latestDtos || latestDtos.length === 0) {
+    const latest = await this.api.apiFileGet({ uID: result })
+    const latestFile = latest.items?.[0]
+
+    if (!latestFile) {
       throw new Error('File Tag 更新失败')
-    } else return latestDtos[0]
+    }
+
+    return latestFile
   }
 
   private commit(result: FileDto) {
-    const index = this.store.fileList.findIndex((f: FileDto) => f.uid === result.uid)
-    if(index==-1)throw new Error('File Tag 更新失败')
+    const index = this.store.fileList.findIndex(f => f.uid === result.uid)
+    if (index === -1) throw new Error('File Tag 更新失败')
     this.store.fileList[index] = result
   }
 }
