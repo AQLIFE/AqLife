b21e8920d09700d5d092d4af9102a6b5ab19435e
AQ <93851495+AQLIFE@users.noreply.github.com>
2026-09-24 15:58:14 +0800

    fix(web): use repaired Todo store in DevPlan

--- apps/Web/src/components/DevPlan.vue
+++ apps/Web/src/components/DevPlan.vue
@@ -1,38 +1,37 @@
 <script setup lang="ts">
-import { TodoApi } from '@/api';
-// import { devPlanTasks } from '@/data/devPlanTasks'
-import { apiConfiguration } from '@/services/api';
-import { useTodoStore } from '@/stores/todoStore';
-import { Plus } from '@element-plus/icons-vue';
-import { ElButton, ElRow,ElCol } from 'element-plus'
-import { onBeforeMount,ref } from 'vue';
+import { ElButton, ElCol, ElRow } from 'element-plus'
+import { Plus } from '@element-plus/icons-vue'
+import { onBeforeMount, ref } from 'vue'
+import { useTodoStore } from '@/stores/todoStore'
+
 const todoStore = useTodoStore()
 const devPlanTitle = '开发计划'
-const todoApi = new TodoApi(apiConfiguration)
+const isExtend = ref(false)
 
-onBeforeMount(async ()=>{
-   if(todoStore.todoList.length==0){
-    const result  = await todoApi.apiTodoGet()
-    if(result.items)todoStore.todoList = result.items
-}
+onBeforeMount(() => {
+  if (todoStore.todoList.length === 0) {
+    return todoStore.refresh()
+  }
 })
-
-const isExtend = ref<boolean>(false)
-
 </script>
 
 <template>
-    <ElCol id="title">{{ devPlanTitle }}</ElCol>
-    <ElCol id="contents">
-        <ElRow flex="column">
-            <ElCol v-for="item,index in todoStore.todoList" :key="index" class="content-item">
-                <ElCol :span="4" class="serial">{{ index+1 }}</ElCol>
-                <ElCol :span="18">{{ item.desc }}</ElCol>
-                <ElCol :span="2" v-if="item.hasChildren" class="serial"><ElButton :icon="Plus" link @click="isExtend=!isExtend"/></ElCol>
-            </ElCol>
-            
-        </ElRow>
-    </ElCol>
+  <ElCol id="title">{{ devPlanTitle }}</ElCol>
+  <ElCol id="contents">
+    <ElRow flex="column">
+      <ElCol
+        v-for="(item, index) in todoStore.todoList"
+        :key="item.uid ?? index"
+        class="content-item"
+      >
+        <ElCol :span="4" class="serial">{{ index + 1 }}</ElCol>
+        <ElCol :span="18">{{ item.desc }}</ElCol>
+        <ElCol :span="2" v-if="item.hasChildren" class="serial">
+          <ElButton :icon="Plus" link @click="isExtend = !isExtend" />
+        </ElCol>
+      </ElCol>
+    </ElRow>
+  </ElCol>
 </template>
 
 <style scoped>
@@ -46,8 +45,6 @@ const isExtend = ref<boolean>(false)
 .content-item{
     display: flex;padding: 10px;border-bottom: 1px dotted var(--back_color_lv3);
     background-color: var(--back_color_lv1);}
-
-
 .content-item > .serial{display: flex;justify-content: center;align-items: center;}
 .content-item > :deep(.el-col > .el-button){margin: 5%;}
 </style>
